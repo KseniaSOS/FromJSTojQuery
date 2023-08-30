ffe5addf14abb5d07707f5b3918f67cc733c3ee9
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-08-30 13:11:56 +0000

    Add lesson

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
+ 	$("#stream1_btn").click(function() {
  		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream1").addClass('highlight_stream');
-	});
+	});, ""
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
@@ -22,3 +22,19 @@ $(document).ready(function() {
 
 
 }); 
+
+// $('#card-panel-1').click(function(){
+//     $('#card-panel-1').hide('slow');
+// });
+
+// $('#card-panel-2').click(function(){
+//     $('#card-panel-2').hide('medium');
+// });
+
+// $('#card-panel-3').click(function(){
+//     $('#card-panel-3').hide('fast');
+// });
+
+// $('#card-panel-4').click(function(){
+//     $('#card-panel-4').hide(3000);
+// });
